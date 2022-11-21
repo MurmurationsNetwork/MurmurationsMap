99b934f27def6de087a83c5116cb6a6d45b7fd32
TingSyuanWang <vincent@fishboneapps.com>
2022-11-20 20:50:36 -0500

    feat: add react-leaflet and css assets
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ components/map/map.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import L from "leaflet";
+import * as ReactLeaflet from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import styles from "./map.module.css";
+
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+const { MapContainer } = ReactLeaflet;
+
+const Map = ({ children, className, ...rest }) => {
+  let mapClassName = styles.map;
+
+  if (className) {
+    mapClassName = `${mapClassName} ${className}`;
+  }
+
+  useEffect(() => {
+    (async function init() {
+      delete L.Icon.Default.prototype._getIconUrl;
+
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: iconRetinaUrl.src,
+        iconUrl: iconUrl.src,
+        shadowUrl: shadowUrl.src,
+      });
+    })();
+  }, []);
+
+  return (
+    <MapContainer className={mapClassName} {...rest}>
+      {children(ReactLeaflet)}
+    </MapContainer>
+  );
+};
+
+export default Map;
