64846b05617c069dbb7034277d162397bc9e28ae
TingSyuanWang <vincent@fishboneapps.com>
2023-05-17 20:58:49 -0400

    refactor: change variable name in loadschema
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- app/utils/load-schemas.js
+++ app/utils/load-schemas.js
@@ -1,8 +1,8 @@
 export async function loadSchemas() {
   try {
     const res = await fetch(process.env.LIBRARY_URL + "/v2/schemas");
-    const schemasJson = await res.json();
-    return schemasJson?.data.map((item) => item.name);
+    const schemas = await res.json();
+    return schemas?.data.map((item) => item.name);
   } catch (e) {
     throw new Response(`loadSchemas error: ${e}`, {
       status: 500,
