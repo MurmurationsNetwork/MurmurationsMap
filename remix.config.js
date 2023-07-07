0cbfc528dd7963d806d96f275d1c9ebf84d62da0
TingSyuanWang <vincent@fishboneapps.com>
2023-07-07 10:02:59 -0400

    fix: fix vercel error
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildPath: 'api/index.js',
   serverMainFields: ['main', 'module'],
   serverModuleFormat: 'cjs',
   serverPlatform: 'node',
